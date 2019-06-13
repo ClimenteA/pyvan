acd68a53c2057bb4f9948fa2ed8698a290fd7546
alincmt <aclimente@ymail.com>
2019-06-13 10:50:13 +0300

    Update main.py

--- example/main.py
+++ example/main.py
@@ -1,33 +1,3 @@
-import sys, os
-if sys.executable.endswith('pythonw.exe'):
-  sys.stdout = open(os.devnull, 'w')
-  sys.stderr = open(os.path.join(os.getenv('TEMP'), 'stderr-{}'.format(os.path.basename(sys.argv[0]))), "w")
-
-import sys, os
-if sys.executable.endswith('pythonw.exe'):
-  sys.stdout = open(os.devnull, 'w')
-  sys.stderr = open(os.path.join(os.getenv('TEMP'), 'stderr-{}'.format(os.path.basename(sys.argv[0]))), "w")
-
-import sys, os
-if sys.executable.endswith('pythonw.exe'):
-  sys.stdout = open(os.devnull, 'w')
-  sys.stderr = open(os.path.join(os.getenv('TEMP'), 'stderr-{}'.format(os.path.basename(sys.argv[0]))), "w")
-
-import sys, os
-if sys.executable.endswith('pythonw.exe'):
-  sys.stdout = open(os.devnull, 'w')
-  sys.stderr = open(os.path.join(os.getenv('TEMP'), 'stderr-{}'.format(os.path.basename(sys.argv[0]))), "w")
-
-import sys, os
-if sys.executable.endswith('pythonw.exe'):
-  sys.stdout = open(os.devnull, 'w')
-  sys.stderr = open(os.path.join(os.getenv('TEMP'), 'stderr-{}'.format(os.path.basename(sys.argv[0]))), "w")
-
-import sys, os
-if sys.executable.endswith('pythonw.exe'):
-  sys.stdout = open(os.devnull, 'w')
-  sys.stderr = open(os.path.join(os.getenv('TEMP'), 'stderr-{}'.format(os.path.basename(sys.argv[0]))), "w")
-
 from flask import Flask  
 from flask import render_template
 from flaskwebgui import FlaskUI
